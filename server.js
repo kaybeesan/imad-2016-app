b961a9e7fe7b46b7c655616fa91a026e6a362166
kaybeesan <kaybeesan@gmail.com>
2016-10-26 21:42:14 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -163,7 +163,6 @@ app.get('/', function (req, res) {
 
 
 app.get('/articles/:articleName', function (req, res) {
-    
     pool.query("SELECT * FROM article WHERE title = '" + req.params.articleName + "'", function (err,result){
         if (err) {
             res.status(500),send(err.toString());
