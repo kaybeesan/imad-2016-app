d959ef3b64c130a6fbc6589655ce7c90ac4711d1
kaybeesan <kaybeesan@gmail.com>
2016-10-27 09:37:47 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -164,18 +164,22 @@ app.get('/', function (req, res) {
 
 app.get('/articles/:articleName', function (req, res) {
 pool.query("SELECT * FROM article WHERE title = '" + req.params.articleName+ "'" , function (err,result){
-          
+    
         if (err) {
-            res.status(500).send(err.toString());
+        res.status(500).send(err.toString());
+        } else {
+        if (result.rows.length === 0) {
+            res.status(404).send('Article not found');
+        } else {
+            var articleData = result.rows[0];
+            res.send(createTemplate(articleData));
         }
-        else { 
-              if (result.rows.length === 0) {
-                  res.status(404).send('Article Not Found!!');
-              } else {
-                  var aticleData = result.rows[0];
-                  res.send(createTemplate(articleData));
-              }
+          
+        
         }
+        
+        
+        
     });
 
 });
